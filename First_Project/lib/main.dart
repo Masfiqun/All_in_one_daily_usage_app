77bef95c70bfa01647ea020e5c3aa885852301e1
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-01-04 02:15:27 +0600

    day 11th 0.2

--- First_Project/lib/main.dart
+++ First_Project/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(primarySwatch: Colors.green),
       darkTheme: ThemeData(primarySwatch: Colors.blue),
       color: Colors.blue,
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
        
     );
   }
