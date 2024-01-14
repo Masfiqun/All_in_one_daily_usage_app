0ded98ea3e02e95e5c26c7b3f9014c3719283c5d
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-01-15 02:16:36 +0600

    day 21th

--- First_Project/lib/Timer/Pages/timer_page.dart
+++ First_Project/lib/Timer/Pages/timer_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class timer_page extends StatefulWidget {
@@ -25,20 +27,53 @@ class _timer_pageState extends State<timer_page> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: ElevatedButton(
-          onPressed: _showTimePicker,
-          style: ElevatedButton.styleFrom(
-            elevation: 20,
-            shadowColor: Colors.amberAccent,
-            surfaceTintColor: Colors.deepPurple,
-            primary: Colors.black
+        child:ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.black,
+                        Colors.amberAccent,
+                        Colors.tealAccent,
+                        Colors.black,
+                      ]
+                    )
+                  ),
+                )
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Gradiant'),
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(15),
+                  primary: Colors.white,
+                  textStyle: const TextStyle(fontSize: 20)
+                ),
+              )
+            ],
           ),
-          child: const Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Text('PICK TIME', style: TextStyle(color: Colors.teal, fontSize: 30, ),),
-          ),
-        ),
+        ) 
       ),
     );
   }
 }
+
+// Center(
+      //   child: ElevatedButton(
+      //     onPressed: _showTimePicker,
+      //     style: ElevatedButton.styleFrom(
+      //       elevation: 20,
+      //       shadowColor: Colors.amberAccent,
+      //       surfaceTintColor: Colors.deepPurple,
+      //       primary: Colors.black
+      //     ),
+      //     child: const Padding(
+      //       padding: EdgeInsets.all(20.0),
+      //       child: Text('PICK TIME', style: TextStyle(color: Colors.teal, fontSize: 30, ),),
+      //     ),
+      //   ),
+      // ),
